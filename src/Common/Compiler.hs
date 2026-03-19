c2e9414250acc39f75bf538a9ef4ca9cff8149d3
LogicMason <33540416+LogicMason5@users.noreply.github.com>
2026-03-19 19:10:56 +0900

    Optimize

--- src/Common/Compiler.hs
+++ src/Common/Compiler.hs
@@ -1,45 +1,55 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE NumericUnderscores  #-}
+{-# LANGUAGE NumericUnderscores #-}
 
 module Common.Compiler (writeRedeemer, mpa) where
 
+-- Cardano / Plutus
 import           Cardano.Api
 import           Plutus.V1.Ledger.Address
 import           Plutus.V1.Ledger.Credential
 import           Plutus.V1.Ledger.Value
 import           PlutusTx.Prelude           hiding (Semigroup (..), unless)
-import           Prelude                    (IO)
-import           Common.Utils
-import           Common.TypesV1           
-import           Wallet.Emulator.Wallet 
+
+-- Ledger / Wallet
 import           Ledger                     hiding (mint, singleton)
 import           Ledger.Ada                 as Ada
-import           Common.UtilsV1             as U
+import           Wallet.Emulator.Wallet     (Wallet, knownWallet, mockWalletPaymentPubKeyHash)
 
+-- Project modules
+import           Common.TypesV1
+import           Common.Utils
+import qualified Common.UtilsV1             as U
 
+-- Standard Prelude (minimal)
+import           Prelude                    (IO)
 
+--------------------------------------------------------------------------------
+-- Helpers
+--------------------------------------------------------------------------------
 
 wallet :: Integer -> Wallet
 wallet = knownWallet
 
 pkh :: Integer -> PaymentPubKeyHash
-pkh x = mockWalletPaymentPubKeyHash $ wallet x
+pkh = mockWalletPaymentPubKeyHash . wallet
+
+--------------------------------------------------------------------------------
+-- Default Mint Parameters
+--------------------------------------------------------------------------------
 
 mpa :: MintParams
-mpa =  MintParams
-    {
-        treasury = pkh 1,
-        referral = pkh 1,
-        referralTx = [          
-
-        ],
-        mpAdaAmount = 200000000     
+mpa = MintParams
+    { treasury   = pkh 1
+    , referral   = pkh 1
+    , referralTx = []
+    , mpAdaAmount = 200_000_000
     }
 
-writeRedeemer  :: MintParams ->  IO ()
-writeRedeemer mp = writeJSON "output/redeemer.json" mp
+--------------------------------------------------------------------------------
+-- IO
+--------------------------------------------------------------------------------
 
-
-
-
+writeRedeemer :: MintParams -> IO ()
+writeRedeemer =
+    writeJSON "output/redeemer.json"
