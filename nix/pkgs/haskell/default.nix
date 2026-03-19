c9f905cf9685f68caf08af98eac885b883b5b068
Trust AI <stackweaver7@gmail.com>
2026-03-19 19:08:45 +0900

    Update default.nix

--- nix/pkgs/haskell/default.nix
+++ nix/pkgs/haskell/default.nix
@@ -6,32 +6,46 @@
 , libsodium-vrf
 , source-repo-override
 }:
-let
-  # The Hackage index-state from cabal.project
-  index-state =
-    let
-      parseIndexState = rawCabalProject:
-        let
-          indexState = lib.lists.concatLists (
-            lib.lists.filter (l: l != null)
-              (map (l: builtins.match "^index-state: *(.*)" l)
-                (lib.splitString "\n" rawCabalProject)));
-        in
-        lib.lists.head (indexState ++ [ null ]);
-    in
-    parseIndexState (builtins.readFile ../../../cabal.project);
 
-  # The haskell project created by haskell-nix.cabalProject'
+let
+  # --- Helpers ---------------------------------------------------------------
+
+  # Extract index-state from cabal.project
+  parseIndexState = content:
+    let
+      matches =
+        map (line: builtins.match "^index-state: *(.*)" line)
+          (lib.splitString "\n" content);
+
+      filtered = lib.filter (x: x != null) matches;
+    in
+      lib.head (filtered ++ [ null ]);
+
+  # --- Values ----------------------------------------------------------------
+
+  cabalProjectContent = builtins.readFile ../../../cabal.project;
+
+  index-state = parseIndexState cabalProjectContent;
+
   project = import ./haskell.nix {
-    inherit lib haskell-nix compiler-nix-name gitignore-nix libsodium-vrf source-repo-override;
+    inherit
+      lib
+      haskell-nix
+      compiler-nix-name
+      gitignore-nix
+      libsodium-vrf
+      source-repo-override;
   };
 
-  # All the packages defined by our project, including dependencies
   packages = project.hsPkgs;
 
-  # Just the packages in the project
-  projectPackages = haskell-nix.haskellLib.selectProjectPackages packages;
-in
-rec {
-  inherit project projectPackages packages;
+  projectPackages =
+    haskell-nix.haskellLib.selectProjectPackages packages;
+
+in rec {
+  inherit
+    index-state
+    project
+    packages
+    projectPackages;
 }
